90aa850d11996b8441059138c20ae6c164a4fb64
”FartDraft” <fartdraft@gmail.com>
2023-08-03 01:14:40 +0300

    +nvim-cmp

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -97,7 +97,9 @@ require("lazy").setup({
             },
             indent = { enable = true },
             ensure_installed = {
-                "bash", "vim", "vimdoc", "lua", "luadoc", "luap",
+                "bash", "json", "yaml", "query",
+                "vim", "vimdoc",
+                "lua", "luadoc", "luap",
                 "markdown", "markdown_inline",
                 "python", "regex",
                 "c", "cpp",
@@ -142,5 +144,114 @@ require("lazy").setup({
             require("nvim-treesitter.configs").setup(opts)
         end,
     },
-
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            { "williamboman/mason.nvim", opts = {} },
+            {
+                "williamboman/mason-lspconfig.nvim",
+                opts = {
+                    ensure_installed = {
+                        "lua_ls", "clangd", "pyright",
+                    },
+                },
+            },
+            { "folke/neodev.nvim", opts = {} },
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        opts = {
+            -- !
+        },
+        config = function()
+            local lspconfig = require('lspconfig')
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.lua_ls.setup {
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        workspace = {
+                            checkThirdParty = false,
+                        },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                    },
+                },
+            }
+            lspconfig.pyright.setup { capabilities = capabilities, }
+            lspconfig.clangd.setup { capabilities = capabilities, }
+        end,
+    },
+    -- Snippets
+    {
+        "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+        },
+        keys = {
+            {
+                "<tab>",
+                function()
+                    return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+                end,
+                expr = true, silent = true, mode = "i",
+            },
+            { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+            { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+        },
+    },
+    -- Powerful completion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        opts = function()
+            local cmp = require("cmp")
+            return {
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-n>"] = cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert
+                    }),
+                    ["<C-p>"] = cmp.mapping.select_prev_item({
+                        behavior = cmp.SelectBehavior.Insert
+                    }),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                formatting = {
+                    format = function(_, item)
+                        local icons = require("core.config").kinds
+                        item.kind = (icons[item.kind] or "") .. item.kind
+                        return item
+                    end,
+                },
+            }
+        end,
+    },
 })
