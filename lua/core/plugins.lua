684e805c7db1db41e2a8a199c07832bad0864f19
”FartDraft” <fartdraft@gmail.com>
2023-08-02 03:14:31 +0300

    +treesitter

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -75,4 +75,68 @@ require("lazy").setup({
         event = "InsertEnter",
         opts = { map_cr = true, },
     },
+    -- Almighty syntax, smart selection and
+    -- new text objects (class, function, call, parameter)
+    {
+        "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" }, cmd = { "TSUpdateSync" },
+        keys = {
+            { "<C-space>", desc = "Increment selection" },
+            { "<bs>", mode = "x", desc = "Decrement selection" },
+            { "<leader>a", desc = "Swap argument forward" },
+            { "<leader>A", desc = "Swap argument back" },
+        },
+        dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+        opts = {
+            auto_install = false,
+            highlight = {
+                enable = true, additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash", "vim", "vimdoc", "lua", "luadoc", "luap",
+                "markdown", "markdown_inline",
+                "python", "regex",
+                "c", "cpp",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    include_surrounding_whitespace = true,
+                    keymaps = {
+                        ["ao"] = "@class.outer",
+                        ["io"] = "@class.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@call.outer",
+                        ["ic"] = "@call.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>a"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>A"] = "@parameter.inner",
+                    },
+                },
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    },
 })
