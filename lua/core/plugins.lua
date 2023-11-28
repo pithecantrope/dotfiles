65894c911e26500c08305d9f3673486342fb5876
”FartDraft” <fartdraft@gmail.com>
2023-11-28 23:09:12 +0300

    +lspkind -icons.lua +prettier nvim-cmp

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -297,11 +297,26 @@ require("lazy").setup({
                 end,
             })
 
-            local hl
-            for type, icon in pairs(require("core.icons").diagnostics) do
-                hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-            end
+            vim.fn.sign_define("DiagnosticSignError", {
+                text = "",
+                texthl = "DiagnosticSignError",
+                numhl = "DiagnosticSignError",
+            })
+            vim.fn.sign_define("DiagnosticSignWarn", {
+                text = "",
+                texthl = "DiagnosticSignWarn",
+                numhl = "DiagnosticSignWarn",
+            })
+            vim.fn.sign_define("DiagnosticSignHint", {
+                text = "",
+                texthl = "DiagnosticSignHint",
+                numhl = "DiagnosticSignHint",
+            })
+            vim.fn.sign_define("DiagnosticSignInfo", {
+                text = "",
+                texthl = "DiagnosticSignInfo",
+                numhl = "DiagnosticSignInfo",
+            })
         end,
     },
     -- Powerful completion
@@ -317,54 +332,130 @@ require("lazy").setup({
                 "Exafunction/codeium.nvim",
                 opts = {},
             },
+            -- Pictograms
+            "onsails/lspkind.nvim",
         },
         opts = function()
             local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            local compare = require("cmp.config.compare")
+            compare.locality.lines_count = 300
+
             return {
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
                 snippet = {
                     expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
+                        luasnip.lsp_expand(args.body)
                     end,
                 },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-n>"] = cmp.mapping.select_next_item({
+                window = {
+                    completion = {
+                        border = "rounded",
+                        winhighlight = "NormalFloat:FloatBorder,CursorLine:Visual,Search:None",
+                        col_offset = -3,
+                        side_padding = 1,
+                        scrollbar = false,
+                    },
+                    documentation = {
+                        border = "rounded",
+                        scrollbar = false,
+                        winhighlight = "NormalFloat:FloatBorder,CursorLine:Visual,Search:None",
+                    },
+                },
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                    keyword_length = 1,
+                },
+                matching = {
+                    disallow_fuzzy_matching = true,
+                    disallow_fullfuzzy_matching = true,
+                    disallow_partial_fuzzy_matching = true,
+                    disallow_partial_matching = false,
+                    disallow_prefix_unmatching = true,
+                },
+                sorting = {
+                    priority_weight = 2,
+                    comparators = {
+                        compare.offset,
+                        compare.exact,
+                        compare.score,
+                        compare.recently_used,
+                        compare.locality,
+                        compare.kind,
+                        compare.length,
+                        compare.order,
+                    },
+                },
+                mapping = {
+                    ["<C-k>"] = cmp.mapping.select_prev_item({
                         behavior = cmp.SelectBehavior.Insert,
                     }),
                     ["<C-p>"] = cmp.mapping.select_prev_item({
                         behavior = cmp.SelectBehavior.Insert,
                     }),
-                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
+                    ["<C-j>"] = cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    }),
+                    ["<C-n>"] = cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert,
+                    }),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(4),
+                    ["<CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    }),
+                },
+                formatting = {
+                    fields = { "kind", "abbr", "menu" },
+                    format = require("lspkind").cmp_format({
+                        mode = "symbol",
+                        maxwidth = 40,
+                        ellipsis_char = "...",
+                        symbol_map = {
+                            Codeium = "",
+                            Module = "",
+                            Operator = "",
+                            Property = "",
+                            TypeParameter = "T",
+                        },
+                    }),
+                },
                 sources = cmp.config.sources({
-                    { name = "codeium" },
+                    { name = "codeium", group_index = 2 },
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
-                    { name = "buffer" },
+                    {
+                        name = "buffer",
+                        option = {
+                            get_bufnrs = function()
+                                local bufs = {}
+                                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                                    bufs[vim.api.nvim_win_get_buf(win)] = true
+                                end
+                                return vim.tbl_keys(bufs)
+                            end,
+                            indexing_interval = 100,
+                            indexing_batch_size = 1000,
+                            max_indexed_line_length = 1024 * 40,
+                            keyword_pattern = [[\k\+]],
+                        },
+                    },
                     { name = "path" },
                 }),
-                formatting = {
-                    format = function(_, item)
-                        local icons = require("core.icons").kinds
-                        item.kind = (icons[item.kind] or "") .. item.kind
-                        return item
-                    end,
+                performance = {
+                    debounce = 60,
+                    throttle = 30,
+                    fetching_timeout = 500,
+                    confirm_resolve_timeout = 500,
+                    async_budget = 1,
+                    max_view_entries = 200,
+                },
+                experimental = {
+                    ghost_text = {
+                        hl_group = "LspCodeLens",
+                    },
                 },
             }
-        end,
-        config = function(_, opts)
-            local cmp = require("cmp")
-            cmp.setup(opts)
-            cmp.event:on(
-                "confirm_done",
-                require("nvim-autopairs.completion.cmp").on_confirm_done()
-            )
         end,
     },
     -- Snippets
