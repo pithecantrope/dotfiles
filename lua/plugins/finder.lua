33347f83a3a1302069749a1f01f17968a0e0112d
”FartDraft” <fartdraft@gmail.com>
2024-02-05 13:48:10 +0300

    Updates

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -6,7 +6,6 @@ return {
         return {
             { "<leader>ff", builtin.find_files, desc = "Find files by name" },
             { "<leader>fg", builtin.live_grep, desc = "Find files by content" },
-            { "<leader>fm", builtin.man_pages, desc = "Find man pages" },
             { "<leader>fh", builtin.help_tags, desc = "Find nvim help" },
         }
     end,
