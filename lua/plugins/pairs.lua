0dfb7cbefbf21909e942a7b49a51332bdd6017b8
”FartDraft” <fartdraft@gmail.com>
2024-03-20 21:16:42 +0300

    fix

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -9,13 +9,14 @@ return {
         keys = {
             { "ys", desc = "Add surround" },
             { "yS", desc = "Add surround on new lines" },
-            -- yss, ySS - Current line
+            -- yss - Current line
             { "cs", desc = "Change surround" },
             { "ds", desc = "Delete surround" },
-            { "<C-s>", mode = "x", desc = "Add surround (visual)" },
+            { "<C-s>", mode = { "x" }, desc = "Add surround (visual)" },
             -- t - tag's type
             -- T - tag's content
             -- f - function call
+            -- i - input
             -- b - )
             -- r - ]
             -- q - {', ", `}
