8f07453317f1048415f0cedada68a95af26afae3
pithecantrope <fartdraft@gmail.com>
2025-02-06 03:22:16 +0300

    +u8is_xxx

--- system/samples/c/src/core.h
+++ system/samples/c/src/core.h
@@ -38,6 +38,73 @@ typedef struct {
 #define PTR [static 1]
 #define INLINE static inline
 
+// u8 ----------------------------------------------------------------------------------------------
+INLINE bool
+u8is_digit(u8 c) {
+        return '0' <= c && c <= '9';
+}
+
+INLINE bool
+u8is_upper(u8 c) {
+        return 'A' <= c && c <= 'Z';
+}
+
+INLINE bool
+u8is_lower(u8 c) {
+        return 'a' <= c && c <= 'z';
+}
+
+INLINE bool
+u8is_alpha(u8 c) {
+        return u8is_upper(c) || u8is_lower(c);
+}
+
+INLINE bool
+u8is_alnum(u8 c) {
+        return u8is_digit(c) || u8is_alpha(c);
+}
+
+INLINE bool
+u8is_xdigit(u8 c) {
+  return u8is_digit(c) || ('A' <= c && c <= 'F') || ('a' <= c && c <= 'f');
+}
+
+INLINE bool
+u8is_blank(u8 c) {
+        return c == ' ' || c == '\t';
+}
+
+INLINE bool
+u8is_space(u8 c) {
+        return u8is_blank(c) || c == '\r' || c == '\n' || c == '\f' || c == '\v';
+}
+
+INLINE bool
+u8is_ascii(u8 c) {
+        return c <= 127;
+}
+
+INLINE bool
+u8is_cntrl(u8 c) {
+        return c <= 31 || c == 127;
+}
+
+INLINE bool
+u8is_print(u8 c) {
+        return 32 <= c && c <= 126;
+}
+
+INLINE bool
+u8is_graph(u8 c) {
+        return 33 <= c && c <= 126;
+}
+
+INLINE bool
+u8is_punct(u8 c) {
+        return u8is_graph(c) && !u8is_alnum(c);
+}
+
+// s8 ----------------------------------------------------------------------------------------------
 INLINE i32
 s8cmp(const s8 a PTR, const s8 b PTR) {
         if (a->len != b->len) {
@@ -47,6 +114,18 @@ s8cmp(const s8 a PTR, const s8 b PTR) {
 }
 
 INLINE bool
-s8equals(const s8 a PTR, const s8 b PTR) {
-        return a->len == b->len && memcmp(a->data, b->data, (size_t)a->len) == 0;
+s8eq(const s8 a PTR, const s8 b PTR) {
+        return s8cmp(a, b) == 0;
 }
+
+// INLINE i32
+// s8icmp(const s8 a PTR, const s8 b PTR) {
+//         (void)a;
+//         (void)b;
+//         return 0;
+// }
+
+// INLINE i32
+// s8ieq(const s8 a PTR, const s8 b PTR) {
+//         return s8icmp(a, b) == 0;
+// }
