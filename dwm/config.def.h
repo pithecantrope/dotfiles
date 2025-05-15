6738136fe0792dfa5168ac19325a613be20a9229
pithecantrope <fartdraft@gmail.com>
2025-05-15 23:42:52 +0300

    dwm: tags on uiop and open on a

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -69,7 +69,7 @@ static const char *lock_screen[]  = { "slock", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_a,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_Left,   spawn,          {.v = brightness_dec} },
 	{ MODKEY,                       XK_Right,  spawn,          {.v = brightness_inc} },
@@ -81,7 +81,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_e,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
@@ -99,10 +99,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_u,                      0)
+	TAGKEYS(                        XK_i,                      1)
+	TAGKEYS(                        XK_o,                      2)
+	TAGKEYS(                        XK_p,                      3)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
